534713870fdbc4219fc77e6fa1a1054a10388f6e
nirireads <nirireads@gmail.com>
2024-10-10 15:37:03 +0545

    fixed bug in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,13 +3,13 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
-export const verifyJWT = asyncHandler(async (req, res, next) => {
+export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
-    if (token) {
+    if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
 
